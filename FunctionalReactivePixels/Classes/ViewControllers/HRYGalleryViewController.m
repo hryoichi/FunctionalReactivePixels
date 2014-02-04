a7be3b468c6f8032dd8410ec3f9c91320800884c
Ryoichi Hara <rychhr@gmail.com>
2014-02-04 19:57:45 +0900

    Add HRYGalleryViewController

---
+++ FunctionalReactivePixels/Classes/ViewControllers/HRYGalleryViewController.m
@@ -0,0 +1,38 @@
+//
+//  HRYGalleryViewController.m
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/04.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYGalleryViewController.h"
+
+@interface HRYGalleryViewController ()
+
+@end
+
+@implementation HRYGalleryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

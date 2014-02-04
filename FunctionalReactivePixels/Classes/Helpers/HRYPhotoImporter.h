99c34e2c23f0ca5bdbc9a30827af067044637893
Ryoichi Hara <rychhr@gmail.com>
2014-02-04 21:04:29 +0900

    Add HRYPhotoImporter

---
+++ FunctionalReactivePixels/Classes/Helpers/HRYPhotoImporter.h
@@ -0,0 +1,13 @@
+//
+//  HRYPhotoImporter.h
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/04.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import Foundation;
+
+@interface HRYPhotoImporter : NSObject
+
+@end
---
+++ FunctionalReactivePixels/Classes/Helpers/HRYPhotoImporter.m
@@ -0,0 +1,13 @@
+//
+//  HRYPhotoImporter.m
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/04.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYPhotoImporter.h"
+
+@implementation HRYPhotoImporter
+
+@end

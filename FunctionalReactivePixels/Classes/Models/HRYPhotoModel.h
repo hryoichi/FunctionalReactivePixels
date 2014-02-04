d7b1e3cfeeea34da2297bdbf013d78ba90757308
Ryoichi Hara <rychhr@gmail.com>
2014-02-04 21:01:41 +0900

    Add HRYPhotoModel

---
+++ FunctionalReactivePixels/Classes/Models/HRYPhotoModel.h
@@ -0,0 +1,13 @@
+//
+//  HRYPhotoModel.h
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/04.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import Foundation;
+
+@interface HRYPhotoModel : NSObject
+
+@end
---
+++ FunctionalReactivePixels/Classes/Models/HRYPhotoModel.m
@@ -0,0 +1,13 @@
+//
+//  HRYPhotoModel.m
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/04.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYPhotoModel.h"
+
+@implementation HRYPhotoModel
+
+@end

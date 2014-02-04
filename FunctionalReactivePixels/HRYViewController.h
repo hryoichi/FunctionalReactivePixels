91c7c5c24bf6b16f672326a5439b415cf05910a5
Ryoichi Hara <rychhr@gmail.com>
2014-02-04 19:40:12 +0900

    Initial Commit

---
+++ FunctionalReactivePixels/HRYViewController.h
@@ -0,0 +1,13 @@
+//
+//  HRYViewController.h
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/04.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HRYViewController : UIViewController
+
+@end

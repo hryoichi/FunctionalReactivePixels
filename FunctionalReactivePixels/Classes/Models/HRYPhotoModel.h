41cea8b4b7b37c6db9b734218ca467ad4d48575d
Ryoichi Hara <rychhr@gmail.com>
2014-02-04 21:29:40 +0900

    Update HRYPhotoModel

--- FunctionalReactivePixels/Classes/Models/HRYPhotoModel.h
+++ FunctionalReactivePixels/Classes/Models/HRYPhotoModel.h
@@ -10,4 +10,14 @@
 
 @interface HRYPhotoModel : NSObject
 
+@property (nonatomic, strong) NSString *photoName;
+@property (nonatomic, strong) NSNumber *identifier;
+@property (nonatomic, strong) NSString *photographerName;
+@property (nonatomic, strong) NSNumber *rating;
+@property (nonatomic, strong) NSString *thumbnailURL;
+@property (nonatomic, strong) NSData *thumbnailData;
+@property (nonatomic, strong) NSString *fullsizedURL;
+@property (nonatomic, strong) NSData *fullsizedData;
+@property (nonatomic, assign, getter=isVotedFor) BOOL votedFor;
+
 @end

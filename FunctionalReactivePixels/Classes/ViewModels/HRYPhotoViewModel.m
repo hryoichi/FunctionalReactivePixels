fa35130be82b8c3a7bd0026b1fd8f0fea5fe814f
Ryoichi Hara <rychhr@gmail.com>
2014-02-05 18:04:00 +0900

    Add HRYPhotoViewModel

---
+++ FunctionalReactivePixels/Classes/ViewModels/HRYPhotoViewModel.m
@@ -0,0 +1,13 @@
+//
+//  HRYPhotoViewModel.m
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/05.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYPhotoViewModel.h"
+
+@implementation HRYPhotoViewModel
+
+@end

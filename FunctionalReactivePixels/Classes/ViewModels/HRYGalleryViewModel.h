17b9e92fa8f3ec85acf17166ab323f3dafa3655b
Ryoichi Hara <rychhr@gmail.com>
2014-02-05 17:53:11 +0900

    Add HRYGalleryViewModel

---
+++ FunctionalReactivePixels/Classes/ViewModels/HRYGalleryViewModel.h
@@ -0,0 +1,13 @@
+//
+//  HRYGalleryViewModel.h
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/05.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "RVMViewModel.h"
+
+@interface HRYGalleryViewModel : RVMViewModel
+
+@end
---
+++ FunctionalReactivePixels/Classes/ViewModels/HRYGalleryViewModel.m
@@ -0,0 +1,13 @@
+//
+//  HRYGalleryViewModel.m
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/05.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYGalleryViewModel.h"
+
+@implementation HRYGalleryViewModel
+
+@end

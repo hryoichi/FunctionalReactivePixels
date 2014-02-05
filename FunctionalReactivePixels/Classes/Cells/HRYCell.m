5339c1020f5c46e2421a146cda7cf405176820fb
Ryoichi Hara <rychhr@gmail.com>
2014-02-05 11:21:22 +0900

    Add HRYCell

---
+++ FunctionalReactivePixels/Classes/Cells/HRYCell.m
@@ -0,0 +1,22 @@
+//
+//  HRYCell.m
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/05.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYCell.h"
+
+@implementation HRYCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end

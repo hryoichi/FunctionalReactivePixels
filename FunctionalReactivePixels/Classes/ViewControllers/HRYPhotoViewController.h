736f8b7add2261b536a82124b16ada476dc01552
Ryoichi Hara <rychhr@gmail.com>
2014-02-05 12:35:50 +0900

    Update HRYPhotoViewController

--- FunctionalReactivePixels/Classes/ViewControllers/HRYPhotoViewController.h
+++ FunctionalReactivePixels/Classes/ViewControllers/HRYPhotoViewController.h
@@ -8,6 +8,13 @@
 
 @import UIKit;
 
+@class HRYPhotoModel;
+
 @interface HRYPhotoViewController : UIViewController
+
+@property (nonatomic, assign, readonly) NSInteger photoIndex;
+@property (nonatomic, strong, readonly) HRYPhotoModel *photoModel;
+
+- (instancetype)initWithPhotoModel:(HRYPhotoModel *)photoModel index:(NSInteger)photoIndex;
 
 @end

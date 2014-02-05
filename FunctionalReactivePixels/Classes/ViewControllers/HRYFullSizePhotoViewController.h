967b08d2007bc9893a17b4b3f0e94fedfef4b90b
Ryoichi Hara <rychhr@gmail.com>
2014-02-05 12:43:42 +0900

    Update HRYFullSizePhotoViewController

--- FunctionalReactivePixels/Classes/ViewControllers/HRYFullSizePhotoViewController.h
+++ FunctionalReactivePixels/Classes/ViewControllers/HRYFullSizePhotoViewController.h
@@ -8,6 +8,20 @@
 
 @import UIKit;
 
+@protocol HRYFullSizePhotoViewControllerDelegate;
+
 @interface HRYFullSizePhotoViewController : UIViewController
+
+@property (nonatomic, copy, readonly) NSArray *photoModelArray;
+@property (nonatomic, weak) id <HRYFullSizePhotoViewControllerDelegate> delegate;
+
+- (instancetype)initWithPhotoModels:(NSArray *)photoModelArray
+                  currentPhotoIndex:(NSInteger)photoIndex;
+
+@end
+
+@protocol HRYFullSizePhotoViewControllerDelegate <NSObject>
+
+- (void)userDidScroll:(HRYFullSizePhotoViewController *)viewController toPhotoAtIndex:(NSInteger)index;
 
 @end
--- FunctionalReactivePixels/Classes/ViewControllers/HRYFullSizePhotoViewController.m
+++ FunctionalReactivePixels/Classes/ViewControllers/HRYFullSizePhotoViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "HRYFullSizePhotoViewController.h"
+#import "HRYPhotoModel.h"
+#import "HRYPhotoViewController.h"
 
-@interface HRYFullSizePhotoViewController ()
+@interface HRYFullSizePhotoViewController () <UIPageViewControllerDataSource, UIPageViewControllerDelegate>
+
+@property (nonatomic, copy, readwrite) NSArray *photoModelArray;
+@property (nonatomic, strong) UIPageViewController *pageViewController;
 
 @end
 
@@ -23,16 +28,79 @@
     return self;
 }
 
+- (instancetype)initWithPhotoModels:(NSArray *)photoModelArray currentPhotoIndex:(NSInteger)photoIndex
+{
+    if (self = [super initWithNibName:nil bundle:nil]) {
+        self.photoModelArray = photoModelArray;
+        self.title = [self.photoModelArray[photoIndex] photoName];
+        self.pageViewController =
+        [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll
+                                        navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal
+                                                      options:@{UIPageViewControllerOptionInterPageSpacingKey: @(30)}];
+        self.pageViewController.dataSource = self;
+        self.pageViewController.delegate   = self;
+        [self addChildViewController:self.pageViewController];
+
+        [self.pageViewController setViewControllers:@[[self photoViewControllerForIndex:photoIndex]]
+                                          direction:UIPageViewControllerNavigationDirectionForward
+                                           animated:NO
+                                         completion:nil];
+    }
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+    self.view.backgroundColor = [UIColor blackColor];
+    self.pageViewController.view.frame = self.view.bounds;
+    [self.view addSubview:self.pageViewController.view];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Public
+
+#pragma mark - Public (Override)
+
+#pragma mark - Private
+
+- (HRYPhotoViewController *)photoViewControllerForIndex:(NSInteger)index
+{
+    if (index >= 0 && index < [self.photoModelArray count]) {
+        HRYPhotoModel *photoModel = self.photoModelArray[index];
+        HRYPhotoViewController *photoVC = [[HRYPhotoViewController alloc] initWithPhotoModel:photoModel index:index];
+        return photoVC;
+    }
+    return nil;
+}
+
+#pragma mark - UIPageViewControllerDataSource
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(HRYPhotoViewController *)viewController
+{
+    return [self photoViewControllerForIndex:(viewController.photoIndex - 1)];
+}
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(HRYPhotoViewController *)viewController
+{
+    return [self photoViewControllerForIndex:(viewController.photoIndex + 1)];
+}
+
+#pragma mark - UIPageViewControllerDelegate
+
+- (void)pageViewController:(UIPageViewController *)pageViewController didFinishAnimating:(BOOL)finished previousViewControllers:(NSArray *)previousViewControllers transitionCompleted:(BOOL)completed
+{
+    self.title = [[self.pageViewController.viewControllers.firstObject photoModel] photoName];
+
+    if ([self.delegate respondsToSelector:@selector(userDidScroll:toPhotoAtIndex:)]) {
+        [self.delegate userDidScroll:self
+                      toPhotoAtIndex:[self.pageViewController.viewControllers.firstObject photoIndex]];
+    }
 }
 
 @end

bde544cece64fd08622408359f3f841b4248b2c5
Ryoichi Hara <rychhr@gmail.com>
2014-02-05 11:35:58 +0900

    Update HRYGalleryViewController

--- FunctionalReactivePixels/Classes/ViewControllers/HRYGalleryViewController.m
+++ FunctionalReactivePixels/Classes/ViewControllers/HRYGalleryViewController.m
@@ -8,12 +8,20 @@
 
 #import "HRYGalleryViewController.h"
 #import "HRYGalleryFlowLayout.h"
+#import "HRYPhotoImporter.h"
+#import "HRYCell.h"
+
+static NSString * const kCellIdentifier = @"Cell";
 
 @interface HRYGalleryViewController ()
+
+@property (nonatomic, strong) NSArray *photosArray;
 
 @end
 
 @implementation HRYGalleryViewController
+
+#pragma mark - Lifecycle
 
 - (id)init
 {
@@ -26,11 +34,52 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.title = @"Popular on 500px";
+
+    [self.collectionView registerClass:[HRYCell class] forCellWithReuseIdentifier:kCellIdentifier];
+
+    @weakify(self);
+    [RACObserve(self, photosArray) subscribeNext:^(id x) {
+        @strongify(self);
+        [self.collectionView reloadData];
+    }];
+
+    [self loadPopularPhotos];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Public
+
+#pragma mark - Private
+
+- (void)loadPopularPhotos
+{
+    [[HRYPhotoImporter importPhotos] subscribeNext:^(id x) {
+        self.photosArray = x;
+    } error:^(NSError *error) {
+        NSLog(@"Couldn't fetch photos from 500px: %@", error);
+    }];
+}
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return [self.photosArray count];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    HRYCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kCellIdentifier forIndexPath:indexPath];
+
+    [cell setPhotoModel:self.photosArray[indexPath.row]];
+
+    return cell;
 }
 
 @end

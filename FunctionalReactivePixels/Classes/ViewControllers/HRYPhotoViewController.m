c9a4f76ed8a992c7987926c3a893de83031f8eea
Ryoichi Hara <rychhr@gmail.com>
2014-02-05 12:31:31 +0900

    Add HRYPhotoViewController

---
+++ FunctionalReactivePixels/Classes/ViewControllers/HRYPhotoViewController.m
@@ -0,0 +1,38 @@
+//
+//  HRYPhotoViewController.m
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/05.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYPhotoViewController.h"
+
+@interface HRYPhotoViewController ()
+
+@end
+
+@implementation HRYPhotoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

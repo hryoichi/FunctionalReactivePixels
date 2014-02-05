4d0de9d18a3901c2bcb19cf88526a7fd664d371b
Ryoichi Hara <rychhr@gmail.com>
2014-02-05 11:48:11 +0900

    Add HRYFullSizePhotoViewController

---
+++ FunctionalReactivePixels/Classes/ViewControllers/HRYFullSizePhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  HRYFullSizePhotoViewController.h
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/05.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HRYFullSizePhotoViewController : UIViewController
+
+@end
---
+++ FunctionalReactivePixels/Classes/ViewControllers/HRYFullSizePhotoViewController.m
@@ -0,0 +1,38 @@
+//
+//  HRYFullSizePhotoViewController.m
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/05.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYFullSizePhotoViewController.h"
+
+@interface HRYFullSizePhotoViewController ()
+
+@end
+
+@implementation HRYFullSizePhotoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

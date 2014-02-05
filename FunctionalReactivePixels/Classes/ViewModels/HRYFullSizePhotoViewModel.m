40d312f1257ffcea2407ac0b374d57e799f4cd8c
Ryoichi Hara <rychhr@gmail.com>
2014-02-05 17:10:59 +0900

    Add HRYFullSizePhotoViewModel

---
+++ FunctionalReactivePixels/Classes/ViewModels/HRYFullSizePhotoViewModel.m
@@ -0,0 +1,13 @@
+//
+//  HRYFullSizePhotoViewModel.m
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/05.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYFullSizePhotoViewModel.h"
+
+@implementation HRYFullSizePhotoViewModel
+
+@end

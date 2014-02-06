942f000e756a99acd7ba1b97df8855ca9a6c48c2
Ryoichi Hara <rychhr@gmail.com>
2014-02-06 20:02:00 +0900

    Update HRYFullSizePhotoModelSpec

--- FunctionalReactivePixelsTests/HRYFullSizePhotoViewModelSpec.m
+++ FunctionalReactivePixelsTests/HRYFullSizePhotoViewModelSpec.m
@@ -12,6 +12,13 @@
 #import "HRYPhotoModel.h"
 #import "HRYFullSizePhotoViewModel.h"
 
+@interface HRYFullSizePhotoViewModel ()
+
+// A private method
+- (HRYPhotoModel *)initialPhotoModel;
+
+@end
+
 SpecBegin(HRYFullSizePhotoViewModel)
 
 describe(NSStringFromClass([HRYFullSizePhotoViewModel class]), ^{
@@ -24,6 +31,49 @@ describe(NSStringFromClass([HRYFullSizePhotoViewModel class]), ^{
                                             initialPhotoIndex:initialPhotoIndex];
         EXP_expect(model).to.equal(viewModel.model);
         EXP_expect(initialPhotoIndex).to.equal(viewModel.initialPhotoIndex);
+    });
+
+    it(@"should return nil for an out-of-bounds photo index", ^{
+        NSArray *model = @[[NSObject new]];
+        NSInteger initialPhotoIndex = 0;
+
+        HRYFullSizePhotoViewModel *viewModel =
+        [[HRYFullSizePhotoViewModel alloc] initWithPhotoArray:model
+                                            initialPhotoIndex:initialPhotoIndex];
+        id subzeroModel = [viewModel photoModelAtIndex:-1];
+        EXP_expect(subzeroModel).to.beNil();
+
+        id aboveBoundsModel = [viewModel photoModelAtIndex:model.count];
+        EXP_expect(aboveBoundsModel).to.beNil();
+    });
+
+    it(@"should return the correct model for photoModelAtIndex", ^{
+        id photoModel = [NSObject new];
+        NSArray *model = @[photoModel];
+        NSInteger initialPhotoIndex = 0;
+
+        HRYFullSizePhotoViewModel *viewModel =
+        [[HRYFullSizePhotoViewModel alloc] initWithPhotoArray:model
+                                            initialPhotoIndex:initialPhotoIndex];
+        id returnModel = [viewModel photoModelAtIndex:0];
+        EXP_expect(returnModel).to.equal(photoModel);
+    });
+
+    it(@"should return the correct initial photo model", ^{
+        NSArray *model = @[[NSObject new]];
+        NSInteger initialPhotoIndex = 0;
+
+        HRYFullSizePhotoViewModel *viewModel =
+        [[HRYFullSizePhotoViewModel alloc] initWithPhotoArray:model
+                                            initialPhotoIndex:initialPhotoIndex];
+
+        id mockViewModel = [OCMockObject partialMockForObject:viewModel];
+        [[[mockViewModel expect] andReturn:model[0]] photoModelAtIndex:initialPhotoIndex];
+
+        id returnedObject = [mockViewModel initialPhotoModel];
+        EXP_expect(returnedObject).to.equal([model firstObject]);
+        
+        [mockViewModel verify];
     });
 });
 

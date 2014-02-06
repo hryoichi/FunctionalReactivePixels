a255b3f2dc537a5026676d8885e16cb7f67e9e5f
Ryoichi Hara <rychhr@gmail.com>
2014-02-06 19:34:53 +0900

    Add HRYFullSizePhotoViewModelSpec

---
+++ FunctionalReactivePixelsTests/HRYFullSizePhotoViewModelSpec.m
@@ -0,0 +1,34 @@
+//
+//  HRYFullSizePhotoViewModelSpec.m
+//  FunctionalReactivePixels
+//
+//  Created by Ryoichi Hara on 2014/02/06.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HRYFullSizePhotoViewModelSpec : XCTestCase
+
+@end
+
+@implementation HRYFullSizePhotoViewModelSpec
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
